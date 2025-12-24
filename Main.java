73f77a6b29178448e01f46a3052c9556108c2f70
Famil Eyvazli <f.eyvazli7@gmail.com>
2025-12-24 23:25:31 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,60 +1,39 @@
+package atm;
 import java.util.Scanner;
+import atm.Operations;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    static double BALANCE = 1000;
-
-    static Scanner scanner = new Scanner(System.in);
-
-    public static void withdraw(){
-        System.out.print("Enter ammount: ");
-        double ammount = scanner.nextDouble();
-        System.out.println("Successful!");
-        System.out.println("Balance: " + (BALANCE+ammount));
-    }
-
-    public static void cash(){
-        System.out.print("Enter ammount: ");
-        double ammount = scanner.nextDouble();
-        System.out.println("Successful!");
-        System.out.println("Balance: " + (BALANCE - ammount));
-    }
-
-    public static void balance(){
-        System.out.println("Balance: " + BALANCE);
-    }
-
-    public static void login() {
-        int PIN = 1234;
-
-        int pin;
-
-        int x = 3;
-        while (x > 0) {
-            System.out.print("Enter Pin: ");
-            try {
-                pin = scanner.nextInt();
-
-                if (pin != PIN) {
-                    System.out.println("Wrong Pin!");
-                }
-            }
-            catch (Exception e) {
-                System.out.println("Invalid Pin! Please try again");
-                scanner.nextLine();
-            }
-            x--;
-        }
-        System.out.println("Your account was blocked!");
-    }
+//    static double BALANCE = 1000;
+//
+//    static Scanner scanner = new Scanner(System.in);
+//
+//    public static void withdraw(){
+//        System.out.print("Enter ammount: ");
+//        double ammount = scanner.nextDouble();
+//        System.out.println("Successful!");
+//        System.out.println("Balance: " + (BALANCE+ammount));
+//    }
+//
+//    public static void cash(){
+//        System.out.print("Enter ammount: ");
+//        double ammount = scanner.nextDouble();
+//        System.out.println("Successful!");
+//        System.out.println("Balance: " + (BALANCE - ammount));
+//    }
+//
+//    public static void balance(){
+//        System.out.println("Balance: " + BALANCE);
+//    }
+    public static Scanner scanner = Operations.scanner;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to ATM System");
 
-        login();
+        if (!Operations.login()){
+            return;
+        }
 
         System.out.println("Options");
 
@@ -67,15 +46,15 @@ public class Main {
 
         switch (option){
                         case 1: {
-                            withdraw();
+                            Operations.withdraw();
                             break;
                         }
                         case 2: {
-                            cash();
+                            Operations.cash();
                             break;
                         }
                         case 3: {
-                            balance();
+                            Operations.balance();
                             break;
                         }
                         case 4: {break;}
---
+++ Operations.java
@@ -0,0 +1,57 @@
+package atm;
+
+import java.util.Scanner;
+
+public class Operations {
+
+    static double BALANCE = 1000;
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void withdraw(){
+        System.out.print("Enter ammount: ");
+        double ammount = scanner.nextDouble();
+        System.out.println("Successful!");
+        System.out.println("Balance: " + (BALANCE+ammount));
+    }
+
+    public static void cash(){
+        System.out.print("Enter ammount: ");
+        double ammount = scanner.nextDouble();
+        System.out.println("Successful!");
+        System.out.println("Balance: " + (BALANCE - ammount));
+    }
+
+    public static void balance(){
+        System.out.println("Balance: " + BALANCE);
+    }
+
+    public static boolean login() {
+        int PIN = 1234;
+
+        int pin;
+
+        int x = 3;
+        while (x > 0) {
+            System.out.print("Enter Pin: ");
+            try {
+                pin = scanner.nextInt();
+
+                if (pin != PIN) {
+                    System.out.println("Wrong Pin!");
+                }
+                else{
+                    return true;
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Invalid Pin! Please try again");
+                scanner.nextLine();
+            }
+            x--;
+        }
+        System.out.println("Your account was blocked!");
+        return false;
+    }
+
+}
